72e2a4e72206fe1fd23e3e20385bf3d333658a9d
seongbinko <wnrhd1082@gmail.com>
2024-03-30 14:45:11 +0900

    ✨feat: [PGS] 두 수의 합 구현

---
+++ seongbinko/week_00/PGS_두_수의_합.java
@@ -0,0 +1,15 @@
+package week_00;
+
+import java.util.Arrays;
+
+public class PGS_두_수의_합 {
+
+    public static void main(String[] args) {
+        System.out.println(solution(2,3));
+    }
+
+    public static int solution(int num1, int num2) {
+        return num1 + num2;
+    }
+
+}
