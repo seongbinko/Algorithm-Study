0fe6f330c7a0effc56b2381211de752c23543039
lotu-us <thwjd1356@naver.com>
2024-04-28 17:44:02 +0900

    :poop: feat: [BOJ] 17611 이모스 직각다각형 (못품)

---
+++ lotuus/src/week_03/BOJ_17611_이모스_직각다각형.java
@@ -0,0 +1,22 @@
+package week_03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BOJ_17611_이모스_직각다각형 {
+
+    /*
+
+    * */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+
+
+
+    }
+
+}
