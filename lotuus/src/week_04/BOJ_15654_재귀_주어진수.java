a2452db29a0b8fa75d64932a83feeaaef4eb452f
lotu-us <thwjd1356@naver.com>
2024-05-01 19:28:03 +0900

    :sparkles:  feat: [BOJ] 15654 재귀 중복불가, 주어진수

---
+++ lotuus/src/week_04/BOJ_15654_재귀_주어진수.java
@@ -0,0 +1,66 @@
+package week_04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.StringTokenizer;
+
+public class BOJ_15654_재귀_주어진수 {
+
+    static int N = 0;
+    static int M = 0;
+    static int[] number;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        number = new int[N];
+
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            number[i] = Integer.parseInt(st.nextToken());
+        }
+
+        //정렬
+        Arrays.sort(number);
+
+        StringBuilder sb = new StringBuilder();
+
+        recursion(0, new int[N], new boolean[N], sb);
+        System.out.println(sb);
+    }
+
+    private static void recursion(int cnt, int[] input, boolean[] check, StringBuilder output) {
+
+        if(cnt == M){
+            for (int i = 0; i < M; i++) {
+                 output.append(input[i]).append(" ");
+            }
+            output.append("\n");
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            int num = number[i];
+
+            if(check[i]){
+                continue;
+            }
+
+            input[cnt] = num;
+            check[i] = true;
+
+            recursion(cnt+1, input, check, output);
+
+            check[i] = false;
+        }
+
+    }
+
+}
+
