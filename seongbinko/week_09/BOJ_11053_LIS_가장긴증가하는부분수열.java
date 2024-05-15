6d7ab37cc4687b7ddc178b9a42146f588f1b8ec5
seongbinko <wnrhd1082@gmail.com>
2024-05-15 19:32:27 +0900

    :sparkles: [BOJ] 11053 가장긴증가하는수열

---
+++ seongbinko/week_09/BOJ_11053_LIS_가장긴증가하는부분수열.java
@@ -0,0 +1,35 @@
+package week_09;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ_11053_LIS_가장긴증가하는부분수열 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+        Arrays.fill(dp, 1);
+
+        // 10 20 10 30 20 50
+        // 1  2  1
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 0; i < N; i++) {
+            // 내가 이전 dp 가장 큰값의 숫자보다 크다면 +1 아니라면 그대로
+            for (int j = 0; j < i; j++) {
+                if (arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+
+        System.out.println(Arrays.stream(dp).max().getAsInt());
+    }
+}
