476bfac5f53a1aaf337b8f8f84db1c3cdfedf2a9
lotu-us <thwjd1356@naver.com>
2024-05-20 23:02:37 +0900

    :poop: PGS 하노이의탑 못품!22

--- lotuus/src/PGS_하노이의탑.java
+++ lotuus/src/PGS_하노이의탑.java
@@ -15,13 +15,16 @@ public class PGS_하노이의탑 {
         );
     }
 
+    //하노이의 탑 세개의 기둥 1, 2, 3번
+    static Stack<Integer> stack1 = new Stack<>();
+    static Stack<Integer> stack2 = new Stack<>();
+    static Stack<Integer> stack3 = new Stack<>();
+    static int N;
+
     public static int[][] solution(int n) {
         int[][] answer = {};
 
-        //하노이의 탑 세개의 기둥 1, 2, 3번
-        Stack<Integer> stack1 = new Stack<>();
-        Stack<Integer> stack2 = new Stack<>();
-        Stack<Integer> stack3 = new Stack<>();
+        n = N;
 
         //1번째 기둥에 N개의 원판 입력
         //원판의 크기 입력
@@ -41,52 +44,47 @@ public class PGS_하노이의탑 {
         //이동 경로 기록하기
         List<int[]> moved = new ArrayList<>();
 
-        //3번에 다 채워지면 종료
-        while(stack3.size() != n){
-
-            if(stack3.size() == n){
-                break;
-            }
-
-            boolean moveFirstToSecond = stack2.empty() || (stack1.peek() < stack2.peek());
-            if(moveFirstToSecond){
-                moved.add(new int[]{1, 2});
-                stack2.push(stack1.pop());
-                continue;
-            }
-
-            boolean moveFirstToThree = stack3.empty() || (stack1.peek() < stack3.peek());
-            if(moveFirstToThree){
-                moved.add(new int[]{1, 3});
-                stack3.push(stack1.pop());
-                continue;
-            }
-
-            boolean moveSecondToFirst = stack1.empty() || (stack2.peek() < stack1.peek());
-            if(moveSecondToFirst){
-                moved.add(new int[]{1, 3});
-                stack1.
-                continue;
-            }
-
-            boolean moveSecondToThird = stack3.empty() || (stack2.peek() < stack3.peek());
-            if(moveSecondToThird){
-                moved.add(new int[]{1, 3});
-                continue;
-            }
-        }
-
-        recursion();
-
+        recursion(stack1.peek());
 
 
         //N개의 원판을 3번 원판으로 최소로 옮기는 방법 return
         return answer;
     }
 
-    private static void recursion() {
+    private static void recursion(int weight) {
 
-        //
+        //종료
+        if(stack3.size()== N){
+            return;
+        }
+
+        //첫번째 기둥에 넣을 수 있는 경우
+
+
+        //두번쨰 기둥에 넣는 경우
+        if(stack2.empty() || weight < stack2.peek()){
+            stack2.push(stack1.pop());
+
+            recursion(stack1.peek());
+        }
+
+
+        //세번째 기둥에 넣을 수 있는 경우
+        if(stack3.empty() || weight < stack3.peek()){
+            stack3.push(stack1.pop());
+
+            if(!stack1.empty()){
+                recursion(stack1.peek());
+            }
+            if(!stack2.empty()){
+                recursion(stack2.peek());
+            }
+
+        }
+
+
+        //각 경우의 수 중 가장 작은거
+
 
     }
 
