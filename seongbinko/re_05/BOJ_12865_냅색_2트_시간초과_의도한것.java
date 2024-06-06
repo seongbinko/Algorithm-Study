cb028b5547f9a7975168dba10b9986249861e13f
seongbinko <wnrhd1082@gmail.com>
2024-06-06 20:00:59 +0900

    feat: BOJ_14501_퇴사_2트, BOJ_12865_냅색_2트_시간초과_의도한것

---
+++ seongbinko/re_05/BOJ_12865_냅색_2트_시간초과_의도한것.java
@@ -0,0 +1,49 @@
+package re_05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class BOJ_12865_냅색_2트_시간초과_의도한것 {
+    static int N;
+    static int W;
+    static List<int[]> inputs = new ArrayList<>();
+    static int answer = Integer.MIN_VALUE;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        W = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            inputs.add(new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
+        }
+
+        hanggun(0, 0, 0);
+        System.out.println(answer);
+
+    }
+
+    private static void hanggun(int index, int w, int v) {
+
+        if (w > W) {
+            return;
+        }
+
+        if (index == N) {
+            answer = Math.max(answer, v);
+            return;
+        }
+        int[] ints = inputs.get(index);
+        // 가방에 물건을 넣는다.
+        hanggun(index + 1, w + ints[0], v + ints[1]);
+
+        // 넣지 않는다.
+        hanggun(index + 1, w, v);
+    }
+}
--- seongbinko/re_05/BOJ_14501_퇴사_2트.java
+++ seongbinko/re_05/BOJ_14501_퇴사_2트.java
@@ -3,17 +3,57 @@ package re_05;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class BOJ_14501_퇴사_2트 {
     static int N;
+    static List<int[]> inputs = new ArrayList<>();
+    static int answer = Integer.MIN_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-
+            inputs.add(new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())});
         }
+
+        consult(0, 0);
+        System.out.println(answer);
+
+    }
+
+    private static void consult(int index, int money) {
+        if (index == N) {
+            answer = Math.max(answer, money);
+            return;
+        }
+
+        int[] ints = inputs.get(index);
+        // 1. 한다
+        if (index + ints[0] <= N) {
+            consult(index + ints[0], money + ints[1]);
+        }
+        // 2. 안한다
+        consult(index + 1, money);
+    }
+
+    private static void consult2(int index, int money) {
+        if (index == N) {
+            answer = Math.max(answer, money);
+            return;
+        }
+        if (index > N) {
+            return;
+        }
+
+        int[] ints = inputs.get(index);
+        // 1. 한다
+        consult(index + ints[0], money + ints[1]);
+
+        // 2. 안한다
+        consult(index + 1, money);
     }
 }
